08fb6f85415cacf11d6d0fda15242c6775f803ae
Bminepc <benjamin.wen@outlook.com>
2023-06-23 15:09:53 +0200

    Added Inputs via. Keyboard and standard to be focused on startup

--- src/LWindow.java
+++ src/LWindow.java
@@ -1,7 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class LWindow extends JFrame {
+
+    GraphicsDevice device = GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices()[0];
 
     JPanel p = new JPanel();
     JLabel l = new JLabel("Bananas");
@@ -15,8 +21,31 @@ public class LWindow extends JFrame {
         p.add(l);
         this.add(p, BorderLayout.CENTER);
         setExtendedState(JFrame.MAXIMIZED_BOTH);
+        device.setFullScreenWindow(this);
         p.setBackground(Color.GREEN);
         p.setSize(this.getWidth(),this.getHeight());
         setVisible(true);
+        this.requestFocus();
+        this.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                if(e.getKeyChar() == '\n'){
+                    l.setText("");
+                }else {
+                    l.setText(l.getText() + e.getKeyChar());
+                }
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+
+            }
+        });
+
     }
 }
